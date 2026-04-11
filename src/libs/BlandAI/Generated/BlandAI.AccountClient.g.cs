c2d3741039ba88467f75d819a61e8c0be9e9d099
Konstantin S. <havendv@gmail.com>
2026-04-11 06:04:01 +0400

    feat: Updated OpenAPI spec (#15)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/BlandAI/Generated/BlandAI.AccountClient.g.cs
+++ src/libs/BlandAI/Generated/BlandAI.AccountClient.g.cs
@@ -4,7 +4,7 @@
 namespace BlandAI
 {
     /// <summary>
-    /// Account information and billing.<br/>
+    /// Account information and billing<br/>
     /// If no httpClient is provided, a new one will be created.<br/>
     /// If no baseUri is provided, the default baseUri from OpenAPI spec will be used.
     /// </summary>
