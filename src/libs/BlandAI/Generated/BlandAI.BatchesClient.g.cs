aa43c6b8166e78e27ef3ee67d8a29082c41d1628
Konstantin S. <havendv@gmail.com>
2026-08-14 19:48:37 +0400

    feat: Updated OpenAPI spec (#36)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/BlandAI/Generated/BlandAI.BatchesClient.g.cs
+++ src/libs/BlandAI/Generated/BlandAI.BatchesClient.g.cs
@@ -34,10 +34,17 @@ namespace BlandAI
 
         /// <inheritdoc/>
         public global::BlandAI.AutoSDKClientOptions Options { get; }
+
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::BlandAI.SourceGenerationContext.Default);
+
         /// <summary>
         /// 
         /// </summary>
-        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext { get; set; } = global::BlandAI.SourceGenerationContext.Default;
+        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext
+        {
+            get => JsonSerializerContextProvider.Value;
+            set => JsonSerializerContextProvider = new(() => value);
+        }
 
 
         /// <summary>
